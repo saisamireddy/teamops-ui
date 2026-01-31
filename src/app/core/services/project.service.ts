d1481d91902058fa4d412f44df336db4c6fdb652
sai <saisamireddy01@gmail.com>
2026-01-31 15:02:48 +0530

    projects(ui): bind project lifecycle to auth state

--- src/app/core/services/project.service.ts
+++ src/app/core/services/project.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 export interface Project {
   id: number;
@@ -10,23 +11,39 @@ export interface Project {
 }
 
 @Injectable({ providedIn: 'root' })
-export class ProjectService {
+export class ProjectService implements OnDestroy {
   private projects$ = new BehaviorSubject<Project[]>([]);
+  private authSub: Subscription;
 
-  constructor(private http: HttpClient) {}
-
-  loadProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>('http://127.0.0.1:8000/api/projects/')
-      .pipe(
-        tap(projects => this.projects$.next(projects))
-      );
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) {
+    //  React to login/logout automatically
+    this.authSub = this.auth.authState().subscribe(isAuth => {
+      if (isAuth) {
+        this.loadProjects().subscribe();
+      } else {
+        this.clearProjects();
+      }
+    });
   }
 
-  getProjects(): Observable<Project[]> {
+  loadProjects() {
+    return this.http
+      .get<Project[]>('http://127.0.0.1:8000/api/projects/')
+      .pipe(tap(projects => this.projects$.next(projects)));
+  }
+
+  getProjects() {
     return this.projects$.asObservable();
   }
 
   clearProjects() {
     this.projects$.next([]);
+  }
+
+  ngOnDestroy() {
+    this.authSub.unsubscribe();
   }
 }
